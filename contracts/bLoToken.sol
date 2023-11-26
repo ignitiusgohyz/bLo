6acf0e69e4ab3d48d688126ce71680e15b175a1f
wglee99 <littlereddot40@gmail.com>
2023-11-26 23:05:54 +0800

    feat: [bLoToken] add supply limit and getCredit function

--- contracts/bLoToken.sol
+++ contracts/bLoToken.sol
@@ -12,9 +12,16 @@ contract bLoToken {
         ERC20 e = new ERC20();
         erc20Contract = e;
         owner = msg.sender;
+        supplyLimit = 100000;
     }
 
-    function getCredit() public payable {}
+    // function for contract caller to exchange ETH for bLo
+    function getCredit() public payable {
+        // exchange rate is 0.001 ETH to 1 bLo; 1 ether gets 1000 bLo
+        uint256 amt = (msg.value * 1000) / (10 ** 18);
+        require(erc20Contract.totalSupply() + amt < supplyLimit, "Not enough bLo in total supply");
+        erc20Contract.mint(msg.sender, amt);
+    }
 
     function checkCredit() public view returns (uint256) {
         return erc20Contract.balanceOf(msg.sender);
